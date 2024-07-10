35b92096db7c5c6de4d5919d27867741d652ddb7
Дмитрий <melehan@mail.ru>
2024-07-10 14:39:15 +0300

    Update module_6_3.py

--- module_6_3.py
+++ module_6_3.py
@@ -1,42 +1,36 @@
 class Horse:
-    def __init__(self):
-        x_distance = 0
+    def __init__(self, x_distance=0, sound='Frrr'):
         self.x_distance = x_distance
-        self.sound = 'Frrr'
+        self.sound = sound
+        super().__init__()
 
-    def run(self, dx):  # где dx - изменение дистанции, увеличивает x_distance на dx.
+    def run(self, dx):
         self.x_distance += dx
 
 
 class Eagle:
-    def __init__(self):
-        y_distance = 0
-        self.y_distance = y_distance  # высота полёта
-        self.sound = 'I train, eat, sleep, and repeat'  # звук, который издаёт орёл
+    def __init__(self, y_distance=0, sound='I train, eat, sleep, and repeat'):
+        self.y_distance = y_distance
+        self.sound = sound
 
-    def fly(self, dy):  # изменение дистанции, увеличивает y_distance на dy.
+    def fly(self, dy):
         self.y_distance += dy
 
 
-class Pegasus(Horse, Eagle):  # В этом методе должны запускаться наследованные методы run и fly соответственно
-    def __init__(self):
-        Horse.__init__(self)
-        Eagle.__init__(self)
-
+class Pegasus(Horse, Eagle):
     def move(self, dx, dy):
         self.run(dx)
         self.fly(dy)
 
-    def get_pos(
-            self):  # возвращает текущее положение пегаса в виде кортежа - (x_distance, y_distance) в том же порядке.
+    def get_pos(self):
         return (self.x_distance, self.y_distance)
 
-    def voice(self):  # печатает значение унаследованного атрибута sound
+    def voice(self):
         print(self.sound)
 
 
-print(Pegasus.mro())
 p1 = Pegasus()
+
 print(p1.get_pos())
 p1.move(10, 15)
 print(p1.get_pos())
